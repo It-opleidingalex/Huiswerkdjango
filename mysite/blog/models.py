ba798fba9e8c8d4c8f0c473e264c0938e1dd5192
It-opleidingalex <it-opleidingalex@hotmail.com>
2023-11-08 11:11:05 +0100

    Huiswerk les 4

---
+++ mysite/blog/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Voetbalspeler(models.Model):
+    naam_voetballer = models.CharField(max_length=100)
+    actuele_voetbalclub = models.CharField(max_length=100)
+    auteur = models.CharField(max_length=100)
+    datum_invoer = models.DateTimeField(default=timezone.now)
+    datum_laatste_aanpassing = models.DateTimeField(default=timezone.now)
+
+    def opslaan_en_publiceren(self):
+        self.datum_laatste_aanpassing = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.naam_voetballer
